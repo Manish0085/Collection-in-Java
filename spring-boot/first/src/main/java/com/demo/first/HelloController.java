eb5bdb768723a8efe21f80ee0899de604643c54d
Manish0085 <manishk57107@gmail.com>
2025-10-29 22:55:04 +0530

    Curd Operation in Spring Boot

--- spring-boot/first/src/main/java/com/demo/first/HelloController.java
+++ spring-boot/first/src/main/java/com/demo/first/HelloController.java
@@ -1,7 +1,7 @@
 package com.demo.first;
 
+import com.demo.first.app.User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
